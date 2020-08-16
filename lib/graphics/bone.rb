f80a494caa9af8e661291a743f7532514fae761a
Evgenii Shalyganov <jinbirdy@gmail.com>
2020-08-16 22:57:09 +0300

    slight refactor

--- lib/graphics/bone.rb
+++ lib/graphics/bone.rb
@@ -6,9 +6,6 @@ module Graphics
       @bone = bone
       @x = x
       @y = y
-      @middle_y = @y + Settings.bone_height / 2
-      @right_edge_x = @x + Settings.bone_width
-      @bottom_edge_y = @y + Settings.bone_height
     end
 
     def draw
@@ -30,18 +27,30 @@ module Graphics
 
       def draw_tiles
         Tile.new(@bone.first_value, x: @x, y: @y).draw
-        Tile.new(@bone.second_value, x: @x, y: @middle_y).draw
+        Tile.new(@bone.second_value, x: @x, y: middle_y).draw
       end
 
       def draw_separator
-        Line.new(x1: @x, y1: @middle_y, x2: @right_edge_x, y2: @middle_y, width: Settings.separator_size, color: 'navy', z: 2)
+        Line.new(x1: @x, y1: middle_y, x2: right_edge_x, y2: middle_y, width: Settings.separator_size, color: 'navy', z: 2)
       end
 
       def draw_borders
-        Line.new(x1: @x, y1: @y, x2: @right_edge_x, y2: @y, width: 1, color: 'navy', z: 2)
-        Line.new(x1: @x, y1: @bottom_edge_y, x2: @right_edge_x, y2: @bottom_edge_y, width: 1, color: 'navy', z: 2)
-        Line.new(x1: @x, y1: @y, x2: @x, y2: @bottom_edge_y, width: 1, color: 'navy', z: 2)
-        Line.new(x1: @right_edge_x, y1: @y, x2: @right_edge_x, y2: @bottom_edge_y, width: 1, color: 'navy', z: 2)
+        Line.new(x1: @x, y1: @y, x2: right_edge_x, y2: @y, width: 1, color: 'navy', z: 2)
+        Line.new(x1: @x, y1: bottom_edge_y, x2: right_edge_x, y2: bottom_edge_y, width: 1, color: 'navy', z: 2)
+        Line.new(x1: @x, y1: @y, x2: @x, y2: bottom_edge_y, width: 1, color: 'navy', z: 2)
+        Line.new(x1: right_edge_x, y1: @y, x2: right_edge_x, y2: bottom_edge_y, width: 1, color: 'navy', z: 2)
+      end
+
+      def middle_y
+        @y + Settings.bone_height / 2
+      end
+
+      def right_edge_x
+        @x + Settings.bone_width
+      end
+
+      def bottom_edge_y
+        @y + Settings.bone_height
       end
   end
 end
