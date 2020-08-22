1ea7d652323a0724dfff992bca5e5702de145df9
enjaku4 <enjaku4@gmail.com>
2020-08-23 02:02:43 +0300

    more pleasant selection color

--- lib/graphics/clickable_area.rb
+++ lib/graphics/clickable_area.rb
@@ -9,7 +9,7 @@ module Graphics
       @z = 1
       @width = Store::Settings.bone_width
       @height = Store::Settings.bone_height
-      self.color = @bone.selected? ? 'red' : 'aqua'
+      self.color = @bone.selected? ? 'yellow' : 'aqua'
       update_coords(@x, @y, @width, @height)
       add
     end
